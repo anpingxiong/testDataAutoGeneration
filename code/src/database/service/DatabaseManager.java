eb71f3051d176603971fc70d989ba282c2f6f93a
anping <974984076@qq.com>
2014-08-07 16:43:09 +0800

    add

---
+++ code/src/database/service/DatabaseManager.java
@@ -0,0 +1,27 @@
+package database.service;
+
+import database.po.Database;
+
+public interface DatabaseManager {
+	/**
+	 * @param database
+	 * anping
+	 * TODO 通过数据库参数对象来生成测试数据
+	 * 上午8:29:35
+	 */
+	public void generationDataByDatabaseObject(Database database);
+    /**
+     * @param database
+	 * anping
+	 * TODO 清空所有的测试数据
+	 * 上午8:30:57
+	 */
+	public void clearAllTestData(Database database);
+	/**
+	 * @param database
+	 * anping
+	 * TODO重建所有的测试数据
+	 * 上午8:31:19
+	 */
+	public void rebuildTestData(Database database);
+}
